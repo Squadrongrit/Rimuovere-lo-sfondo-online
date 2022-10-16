8a71ea931e26a604bcf27876cb1d7654ccab3f90
IntelligenzaArtificiale <108482353+IntelligenzaArtificiale@users.noreply.github.com>
2022-10-16 12:38:01 +0200

    Add files via upload

--- Home.py
+++ Home.py
@@ -4,15 +4,15 @@ from PIL import Image
 import numpy as np
 
 st.title("rembg Demo")
-images = st.file_uploader("Load Image", accept_multiple_files =True)
+images = st.file_uploader("Load Image", accept_multiple_files =True, type=['png', 'jpg','Jpeg'])
 
 if images:
     col1, col2 = st.columns(2)
     for image in images:
+        col1.header("Originali")
+        col2.header("Senza Sfondo")
         with Image.open(image) as img:
-            col1.header("Original")
             col1.image(img)
 
             output = remove(img)
-            col2.header("Extracted")
             col2.image(output)
