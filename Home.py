b7d1d7fdeeab002d6e497de22386cd1238c9afec
IntelligenzaArtificiale <108482353+IntelligenzaArtificiale@users.noreply.github.com>
2022-10-18 09:34:38 +0200

    Add files via upload

--- Home.py
+++ Home.py
@@ -1,3 +1,4 @@
+import base64
 import streamlit as st
 from rembg import remove
 from PIL import Image
@@ -25,6 +26,16 @@ st.title("Rimuovi gli sfondi automaticamente al 100% in 5 secondi con un clic")
 colb1, colb2 = st.columns(2)
 images = colb2.file_uploader("Trascina una o più immagini", accept_multiple_files =True, type=['png', 'jpg','Jpeg'])
 colb1 = st.markdown("![Alt Text](remove-gif.242b60d.gif)")
+
+file_ = open("remove-gif.242b60d.gif", "rb")
+contents = file_.read()
+data_url = base64.b64encode(contents).decode("utf-8")
+file_.close()
+
+colb1.markdown(
+    f'<img src="data:image/gif;base64,{data_url}" alt="cat gif">',
+    unsafe_allow_html=True,
+)
 
 if images:
     col1, col2 = st.columns(2)
