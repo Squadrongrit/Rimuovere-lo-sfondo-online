102f7700b7e6f0be6e5ef7ffe926b36bb8483677
IntelligenzaArtificiale <108482353+IntelligenzaArtificiale@users.noreply.github.com>
2022-10-18 10:39:57 +0200

    Add files via upload

--- Home.py
+++ Home.py
@@ -14,7 +14,7 @@ style = """
     background-color: #076ad9;
     border-radius: 0.5rem;
     color: rgb(49, 51, 63);
-    height: 300px;
+    height: 400px;
     width: 90%;
     box-shadow: 10px 10px 5px #dedede;
     text-align: center;
@@ -77,16 +77,15 @@ st.markdown(style, unsafe_allow_html=True) #Title rendering
 
 st.title("Rimuovi gli sfondi automaticamente al 100% in 5 secondi con un clic")
 colb1, colb2 = st.columns(2)
-images = colb2.file_uploader("Trascina una o più immagini", accept_multiple_files =True, type=['png', 'jpg','Jpeg'])
+images = colb2.file_uploader("", accept_multiple_files =True, type=['png', 'jpg','Jpeg'])
 
 colb1.image("background-removal-banner-1.jpg")
-st.write("![Your Awsome GIF](https://www.canva.com/design/DAFPYAQyKGs/watch)")
 
 if images:
-    col1, col2 = st.columns(2)
     for image in images:
         with st.spinner("Rimozione del background in corso..."):
             with st.expander("Immagine numero {}".format(images.index(image)+1)):
+                col1, col2 = st.columns(2)
                 col1.header("Originali")
                 col2.header("Senza Sfondo")
                 with Image.open(image) as img:
