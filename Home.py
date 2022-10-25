015d8556fe6b436986345138d3349b6352aa42f5
IntelligenzaArtificiale <108482353+IntelligenzaArtificiale@users.noreply.github.com>
2022-10-25 18:20:12 +0200

    Update Home.py

--- Home.py
+++ Home.py
@@ -10,6 +10,7 @@ import numpy as np
 
 style = """
 <style>
+#root > div:nth-child(1) > div > div > div > div > section > div {padding-top: 0rem;}
 .css-po3vlj {
     display: flex;
     -webkit-box-align: center;
